61794c8086110186393db92ce79ffdf6bbe7344c
Youngdi <u9951062@gmail.com>
2018-06-06 20:43:36 +0800

    修正遊戲規則介紹

--- app/constants/questionWorkshop.js
+++ app/constants/questionWorkshop.js
@@ -3,18 +3,18 @@ exports.questionWorkshop = [
     category: "藏寶地圖",
     name: "",
     introduction:
-      "在第二頁的藏寶地圖上，可以看到關主的位置（紅框）以及寶石（紅圈）的埋藏處；可以藉由闖關或掃QR code來獲得寶石~",
+      "在第二頁的藏寶地圖上，可以看到關主的位置（紅框）以及寶石（紅圈）的埋藏處；可以藉由闖關或掃QR code來獲得寶石~\n\n每一個單一QR code資源點，每小隊只能掃一次，但偶爾會有意外的CD time重置驚喜喔！",
     image: require("../images/gameRule/A.jpg")
   },
   {
     category: "PP island開墾圖",
     name: "",
     introduction:
-      "在第三頁的PP島(II Peter)上，可用相應寶石來購買土地，成功由任一起始堡壘打通至中央遺跡者道路者獲勝!",
+      "在第三頁的PP島(II Peter)上，可用相應寶石來購買土地，成功由任一起始堡壘打通至中央遺跡道路者獲勝!",
     image: require("../images/gameRule/B.jpg")
   },
 　{
-    category: "主頁/寶石換購",
+    category: "主頁－寶石換購",
     name: "",
     introduction:
       "APP第一頁為我們的首頁，如果寶石不夠者，可在此使用K寶石(每隊起手22K)來換購其他寶石\n\n另外，在首頁左上的Menu中有許多我們這次營會的相關資訊喔~",
@@ -26,5 +26,12 @@ exports.questionWorkshop = [
     introduction:
       "另外，來自各國的挑戰者們擁有各種不同的能力加成；可點擊Menu內的國家歷史或首頁形像圖來確認＜開墾．ＰＰ埃冷＞的各國能力。",
     image: require("../images/gameRule/D.jpg")
+  },
+ {
+    category: "各國資源",
+    name: "",
+    introduction:
+      "歸屬於同一國家內的小隊，你們所擁有的資源及土地是共享的；所以，在購地前，請盡量先跟同一國家內的其他小隊碰頭討論一下怎麼分工喔。",
+    image: require("../images/gameRule/E.jpg")
   },
 ];
